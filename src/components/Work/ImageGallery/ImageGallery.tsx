29677b461ad01ab130f326bc02154ab9e32e2302
Frysuni <ibragimzinatullin@mail.ru>
2024-03-15 01:22:58 +0500

    s

--- src/components/Work/ImageGallery/ImageGallery.tsx
+++ src/components/Work/ImageGallery/ImageGallery.tsx
@@ -21,7 +21,6 @@ import {
   THUMBNAIL_SIZE_TOTAL,
 } from "../../../utils/consts";
 import Thumbnails from "./Thumbnails";
-import { getStrapiMedia } from "../../../lib/getStrapiMedia";
 import { CollectionImage } from "../../../types/strapi/Category";
 
 interface ImageGalleryProps {
